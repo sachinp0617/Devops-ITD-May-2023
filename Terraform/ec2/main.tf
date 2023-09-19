6f979fd11f091d43f18052c4a240f9dafe8ad574
Harsha Jain <jaintpharsha@gmail.com>
2023-09-19 10:31:23 +0530

    remote_state 19-September-23 10:31AM

--- Terraform/ec2/main.tf
+++ Terraform/ec2/main.tf
@@ -1,5 +1,3 @@
-
-
 data "aws_ami" "ubuntu" {
   most_recent = true
 
@@ -18,10 +16,42 @@ resource "aws_instance" "my_instance" {
     tags = {
         Name = "First_Terraform_resource_creation"
     } 
-
 }
 
+resource "aws_s3_bucket" "s3_backend" {
+    bucket = var.bucket_name
+}
 
+resource "aws_s3_bucket_server_side_encryption_configuration" "s3_backend_sse" {
+  bucket = aws_s3_bucket.s3_backend.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm     = "AES256"
+    }
+  }
+}
+
+resource "aws_s3_bucket_versioning" "s3_backend_versioning" {
+  bucket = aws_s3_bucket.s3_backend.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+
+  depends_on = [
+    aws_s3_bucket_server_side_encryption_configuration.s3_backend_sse
+  ]
+}
+
+resource "aws_dynamodb_table" "dynamodb_state_locking" {
+    name = var.dynamobd_name
+    billing_mode = "PAY_PER_REQUEST"
+    hash_key = var.hash_key
+    attribute {
+    name = var.hash_key
+    type = "S"
+  }
+}
 
 
 
